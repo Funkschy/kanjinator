4abca6b3bd9843bbc429b9480a0aec80ec9dc3b7
Funkschy <felix.schoeller@protonmail.com>
2022-05-02 17:10:50 +0200

    Switched from TinySegmenter to kuromoji
    Kuromoji uses actual dictionaries to provide a way superior
    classification white splitting japanese sentences. This allows for
    pretty granular filtering by word type and also fixes bugs where we
    looked up parts of a word as separate words, especially verb conjugations

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
                  [net.sourceforge.tess4j/tess4j "5.2.0"]
                  [org.openpnp/opencv "4.5.1-2"]
                  [org.clojure/data.json "2.4.0"]
+                 [com.atilika.kuromoji/kuromoji-ipadic "0.9.0"]
                  [com.github.funkschy/tinysegmenter "0.1.0"]]
   :main ^:skip-aot kanjinator.core
   :target-path "target/%s"
--- src/kanjinator/languages/jp.clj
+++ src/kanjinator/languages/jp.clj
@@ -2,11 +2,46 @@
   (:require
    [kanjinator.language :refer [Language]]
    [kanjinator.preprocess :refer [default-process-pipeline preprocess-image]]
-   [tinysegmenter.core :refer [segment]]
    [kanjinator.dictionaries.jisho :refer [lookup-word-in-dictionary]])
   (:import
+   [com.atilika.kuromoji.ipadic Token Tokenizer]
    [java.awt.image BufferedImage]
    [net.sourceforge.tess4j Tesseract]))
+
+(def ^:private parts-of-speech-1
+  {"名詞"   :noun           ;; めいし
+   "助詞"   :particle       ;; じょし
+   "形容詞" :adjective      ;; けいようし
+   "助動詞" :auxiliary-verb ;; inflecting dependent word = じょどうし (e.g です)
+   "動詞"   :verb})         ;; どうし
+
+(def ^:private parts-of-speech-2
+  {"自立"    :independent             ;; じりつ
+   "非自立"   :dependent              ;; not independent = ひじりつ
+   "接続助詞" :conjugation-particle}) ;;  せつぞくじょし
+
+(defn- part-of-speech-1 [^Token t]
+  (parts-of-speech-1 (.getPartOfSpeechLevel1 t)))
+
+(defn- part-of-speech-2 [^Token t]
+  (parts-of-speech-2 (.getPartOfSpeechLevel2 t)))
+
+(defn- segment [part-wanted? text]
+  (let [tokens (.tokenize (new Tokenizer) text)
+        base   (fn [^Token t] (.getBaseForm t))
+        take?  (fn [^Token t]
+                 (and (part-wanted? (part-of-speech-1 t))
+                      ;; those are just grammatical forms after a verb that might be classified
+                      ;; as separate verbs, e.g. なっている is classified as なっ, て, いる, which
+                      ;; isn't really correct, because it's just the te-iru form of なる
+                      ;; the て is a conjugation-particle and the いる is a dependent verb, so
+                      ;; we can just filter those
+                      (not (#{:dependent :conjugation-particle} (part-of-speech-2 t)))))
+        xf     (comp (filter take?) (map base))]
+    ;; I know some people hate commented code, but I actually use this for debugging quite frequently
+    ;; (doseq [^Token t tokens]
+    ;;   (println (.getPartOfSpeechLevel1 t) (.getSurface t) (.getBaseForm t) (.getPartOfSpeechLevel2 t)))
+    (transduce xf conj tokens)))
 
 (def ^:private ^Tesseract tesseract-single
   (doto (new Tesseract)
@@ -43,7 +78,7 @@
          (re-seq jp-regex)
          (map first)
          (apply str)
-         (segment)))
+         (segment #{:noun :verb :adjective})))
   (lookup [_ word]
     (lookup-word-in-dictionary word)))
 
