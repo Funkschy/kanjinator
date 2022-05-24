49ead9034d28f2a9266ad42e01325073b8cbaeb5
Funkschy <felix.schoeller@protonmail.com>
2022-05-24 20:04:38 +0200

    added linux buildscript with jlink

---
+++ build-linux.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+output_dir="linux-dist"
+rm -rf "./$output_dir"
+
+echo "-> building custom jre"
+jlink \
+    --add-modules "java.sql,java.datatransfer,java.desktop,java.logging,java.prefs,java.xml,jdk.unsupported" \
+    --output "$output_dir" \
+    --strip-debug \
+    --no-man-pages \
+    --no-header-files \
+    --compress=2
+
+echo "-> building application"
+lein clean
+lein uberjar
+cp ./target/uberjar/*-standalone.jar "$output_dir/kanjinator.jar"
+
+echo "-> creating startup script"
+startup="
+#!/bin/sh
+dir=\"\$(dirname \"\$0\")\"
+\"\$dir/bin/java\" -jar \"\$dir/kanjinator.jar\"
+"
+echo "$startup" > "$output_dir/kanjinator.sh"
+chmod +x "$output_dir/kanjinator.sh"
+
+echo "-> creating tar ball"
+tar -czvf kanjinator.tar.gz -C "$output_dir" .
+rm -rf $output_dir
